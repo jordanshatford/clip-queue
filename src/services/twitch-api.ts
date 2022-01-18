5a8fe36ebbd9649c7f5dc624101d755e82890f18
Jordan Shatford <jordanshatford@live.com>
2022-01-17 20:40:05 -0400

    Memoize games and clip information to reduce fetching

--- src/services/twitch-api.ts
+++ src/services/twitch-api.ts
@@ -5,24 +5,39 @@ import { userStore } from "@/stores/user";
 
 const { baseURL, headers } = config.Twitch.API;
 
+const memoizedGames: Record<string, TwitchGame> = {};
+const memoizedClips: Record<string, TwitchClip> = {};
+
 export default class TwitchAPI {
   private static axiosClient = axios.create({ baseURL, headers });
 
   public static async getClip(id: string): Promise<TwitchClip> {
-    const { data } = await this.axiosClient.get<{ data: TwitchClip[] }>(`clips?id=${id}`, {
-      headers: {
-        Authorization: `Bearer ${userStore.user.accessToken}`,
-      },
-    });
-    return data.data[0];
+    if (id in memoizedClips) {
+      return memoizedClips[id];
+    } else {
+      const { data } = await this.axiosClient.get<{ data: TwitchClip[] }>(`clips?id=${id}`, {
+        headers: {
+          Authorization: `Bearer ${userStore.user.accessToken}`,
+        },
+      });
+      const clip = data.data[0];
+      memoizedClips[id] = clip;
+      return clip;
+    }
   }
 
   public static async getGame(id: string): Promise<TwitchGame> {
-    const { data } = await this.axiosClient.get<{ data: TwitchGame[] }>(`games?id=${id}`, {
-      headers: {
-        Authorization: `Bearer ${userStore.user.accessToken}`,
-      },
-    });
-    return data.data[0];
+    if (id in memoizedGames) {
+      return memoizedGames[id];
+    } else {
+      const { data } = await this.axiosClient.get<{ data: TwitchGame[] }>(`games?id=${id}`, {
+        headers: {
+          Authorization: `Bearer ${userStore.user.accessToken}`,
+        },
+      });
+      const game = data.data[0];
+      memoizedGames[id] = game;
+      return game;
+    }
   }
 }
