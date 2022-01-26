a81762d9a1c76c3a48847c735b9327a61459d905
Jordan Shatford <jordanshatford@live.com>
2022-01-25 20:25:27 -0400

    Add tests for twitch auth

---
+++ tests/unit/services/twitch-auth.spec.ts
@@ -0,0 +1,29 @@
+import TwitchAuth from "@/services/twitch-auth";
+
+describe("twitch-auth.ts", () => {
+  it("redirect the user to the twitch login page", () => {
+    // @ts-ignore
+    delete window.location;
+    // @ts-ignore
+    window.location = { assign: jest.fn() };
+    TwitchAuth.redirectToLogin()
+    expect(window.location.assign).toHaveBeenCalledTimes(1)
+  })
+
+  it("can login the user given an auth hash string", () => {
+    const authHash = "#access_token=token&id_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoiYWRtaW4iLCJpYXQiOjE0MjI3Nzk2Mzh9.gzSraSYS8EXBxLN_oWnFSRgCzcmJmMjLiuyu5CSpyHI&scope=openid+chat%3Aread&token_type=bearer"
+    const authInfo = TwitchAuth.login(authHash);
+    expect(authInfo?.access_token).toEqual("token");
+    // This is a fake token
+    expect(authInfo?.id_token).toEqual("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoiYWRtaW4iLCJpYXQiOjE0MjI3Nzk2Mzh9.gzSraSYS8EXBxLN_oWnFSRgCzcmJmMjLiuyu5CSpyHI");
+    expect(authInfo?.scope).toEqual("openid+chat:read");
+    expect(authInfo?.token_type).toEqual("bearer");
+    expect(authInfo?.decodedIdToken).toEqual({"iat": 1422779638, "loggedInAs": "admin"});
+  })
+
+  it("will return null when invalid auth hash is given", () => {
+    const authHash = "";
+    const authInfo = TwitchAuth.login(authHash);
+    expect(authInfo).toEqual(null);
+  })
+});
