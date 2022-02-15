31a6d69d263d76cc96744abd463410cb4dd66797
Jordan Shatford <jordanshatford@live.com>
2022-02-15 17:09:55 -0400

    style: make queue item card smaller

--- src/components/queue/ClipQueueItem.vue
+++ src/components/queue/ClipQueueItem.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="cq-card max-w-xs overflow-hidden">
+  <div class="cq-card max-w-[16rem] overflow-hidden">
     <div
-      class="flex items-end justify-end h-56 w-full bg-cover"
+      class="flex items-end justify-end h-36 w-full bg-cover"
       :style="'background-image: url(' + clip.thumbnailUrl + ')'"
     >
-      <v-button class="pb-1 mr-3 -mb-2" @click="emit('play')">
-        <v-icon icon="play" class="w-5 h-5" />
+      <v-button class="pb-1 mr-2 -mb-2" @click="emit('play')">
+        <v-icon icon="play" class="w-4 h-4" />
       </v-button>
-      <v-button variant="danger" class="pb-1 mr-3 -mb-2" @click="emit('remove')">
-        <v-icon icon="trash" class="w-5 h-5" />
+      <v-button variant="danger" class="pb-1 mr-2 -mb-2" @click="emit('remove')">
+        <v-icon icon="trash" class="w-4 h-4" />
       </v-button>
     </div>
     <div class="px-5 py-3">
-      <span class="cq-text text-lg mt-2">{{ clip.title }}</span>
+      <span class="cq-text mt-2">{{ clip.title }}</span>
       <h3 class="cq-text-subtle-semibold">
         {{ clip.channel }}<span class="cq-text-subtle"> playing </span>{{ clip.game }}
       </h3>
