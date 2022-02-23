ef6a7763a73d1e9f47bc0b28bcd34b62ea86c3eb
Jordan Shatford <jordanshatford@live.com>
2022-02-23 08:56:00 -0400

    refactor: replace v-formgroup with css classes
    The component was not needed and its just as simple
    to use css classes.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import App from "@/App.vue"
 import router from "@/router"
 import sentry from "@/services/sentry"
 import Alert from "@/components/common/Alert.vue"
-import FormGroup from "@/components/common/FormGroup.vue"
 import Input from "@/components/common/Input.vue"
 import Switch from "@/components/common/Switch.vue"
 import TextArea from "@/components/common/TextArea.vue"
@@ -15,7 +14,6 @@ const app = createApp(App)
 sentry.init(app)
 app.use(createPinia())
 app.component("v-alert", Alert)
-app.component("v-formgroup", FormGroup)
 app.component("v-input", Input)
 app.component("v-switch", Switch)
 app.component("v-textarea", TextArea)
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,97 +1,96 @@
 <template>
   <p class="cq-title">Settings</p>
-  <div class="cq-card mx-auto p-2 max-w-lg mb-2">
-    <v-formgroup>
-      <v-button class="w-full" variant="danger" :disabled="queue.history.empty()" @click="queue.purge()">
-        Purge History
-      </v-button>
-      <label class="cq-text-subtle">Purge all clips previously viewed allowing them to be resubmitted.</label>
-    </v-formgroup>
+  <div class="cq-form mb-2">
+    <v-button class="w-full" variant="danger" :disabled="queue.history.empty()" @click="queue.purge()">
+      Purge History
+    </v-button>
+    <label class="cq-text-subtle">Purge all clips previously viewed allowing them to be resubmitted.</label>
   </div>
-  <form @submit.prevent="onSubmit" @reset="onReset" :key="formKey">
-    <div class="cq-card mx-auto p-2 max-w-lg">
-      <v-formgroup label="Allow chat commands?" class="w-full flex justify-between pr-2 pb-0">
-        <v-switch id="allowCommands" v-model="formSettings.allowCommands" />
-      </v-formgroup>
-      <v-formgroup v-if="formSettings.allowCommands" label="Chat command prefix:">
-        <v-input type="text" required @keydown.space.prevent maxlength="8" v-model="formSettings.commandPrefix" />
-        <div class="text-left pl-1 my-2 cq-text-subtle">
-          <label>The following commands are available to mods: </label>
+  <form @submit.prevent="onSubmit" @reset="onReset" :key="formKey" class="cq-form">
+    <div class="cq-toggle-form-group">
+      <label class="cq-form-group-label">Allow chat commands?</label>
+      <v-switch id="allowCommands" v-model="formSettings.allowCommands" />
+    </div>
+    <div v-if="formSettings.allowCommands">
+      <label class="cq-form-group-label">Chat command prefix:</label>
+      <v-input type="text" required @keydown.space.prevent maxlength="8" v-model="formSettings.commandPrefix" />
+      <div class="cq-text-subtle text-left pl-1 my-2">
+        <label>The following commands are available to mods: </label>
+        <ul class="list-disc pl-8">
+          <li v-for="(item, key) in commands.help" :key="key">
+            <span class="cq-text-subtle-semibold">{{ `${settings.commandPrefix}${key}` }}</span
+            >: {{ item }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div>
+      <div class="cq-toggle-form-group">
+        <label class="cq-form-group-label">Auto remove clips on moderation?</label>
+        <v-switch id="autoRemoveClips" v-model="formSettings.autoRemoveClips" />
+      </div>
+      <div v-if="formSettings.autoRemoveClips" class="text-left pl-1 cq-text-subtle mb-2">
+        <label>
+          When a user has their chat message deleted, is timed out, or banned, then the clips they submitted will be
+          removed.
+        </label>
+      </div>
+    </div>
+    <div class="cq-toggle-form-group">
+      <label class="cq-form-group-label">Send bot messages in chat?</label>
+      <v-switch id="sendMsgsInChat" v-model="formSettings.sendMsgsInChat" />
+    </div>
+    <div v-if="formSettings.sendMsgsInChat">
+      <div class="cq-toggle-form-group">
+        <label class="cq-form-group-label">Queue open message</label>
+        <v-switch id="sendQueueOpenMsg" v-model="formSettings.sendQueueOpenMsg" />
+      </div>
+      <div v-if="formSettings.sendQueueOpenMsg" class="cq-form-group pt-0">
+        <v-textarea required minlength="3" maxlength="500" v-model="formSettings.queueOpenMsg" />
+      </div>
+      <div class="cq-toggle-form-group">
+        <label class="cq-form-group-label">Queue close message</label>
+        <v-switch id="sendQueueCloseMsg" v-model="formSettings.sendQueueCloseMsg" />
+      </div>
+      <div v-if="formSettings.sendQueueCloseMsg" class="cq-form-group pt-0">
+        <v-textarea required minlength="3" maxlength="500" v-model="formSettings.queueCloseMsg" />
+      </div>
+      <div class="cq-toggle-form-group">
+        <label class="cq-form-group-label">Current clip message</label>
+        <v-switch id="sendCurrentClipMsg" v-model="formSettings.sendCurrentClipMsg" />
+      </div>
+      <div v-if="formSettings.sendCurrentClipMsg">
+        <div class="text-left pl-1 mb-2 cq-text-subtle">
+          <label>The following will be replaced in the message sent to chat:</label>
           <ul class="list-disc pl-8">
-            <li v-for="(item, key) in commands.help" :key="key">
-              <span class="cq-text-subtle-semibold">{{ `${settings.commandPrefix}${key}` }}</span
-              >: {{ item }}
+            <li><span class="cq-text-subtle-semibold">{url}</span>: url of the clip</li>
+            <li><span class="cq-text-subtle-semibold">{title}</span>: title of the clip</li>
+            <li><span class="cq-text-subtle-semibold">{channel}</span>: channel the clip is of</li>
+            <li><span class="cq-text-subtle-semibold">{game}</span>: game in the clip</li>
+            <li>
+              <span class="cq-text-subtle-semibold">{submitter}</span>: chatter who submitted the clip (or reddit
+              poster)
             </li>
           </ul>
         </div>
-      </v-formgroup>
-      <div>
-        <v-formgroup label="Auto remove clips on moderation?" class="w-full flex justify-between pr-2 pb-0">
-          <v-switch id="autoRemoveClips" v-model="formSettings.autoRemoveClips" />
-        </v-formgroup>
-        <div v-if="formSettings.autoRemoveClips" class="text-left pl-1 cq-text-subtle mb-2">
-          <label>
-            When a user has their chat message deleted, is timed out, or banned, then the clips they submitted will be
-            removed.
-          </label>
+        <div class="cq-form-group pt-0">
+          <v-textarea required minlength="3" maxlength="500" v-model="formSettings.currentClipMsg" />
         </div>
       </div>
-      <v-formgroup label="Send bot messages in chat?" class="w-full flex justify-between pr-2">
-        <v-switch id="sendMsgsInChat" v-model="formSettings.sendMsgsInChat" />
-      </v-formgroup>
-      <div v-if="formSettings.sendMsgsInChat">
-        <v-formgroup label="Queue open message" class="w-full flex justify-between pr-2 pb-0">
-          <v-switch id="sendQueueOpenMsg" v-model="formSettings.sendQueueOpenMsg" />
-        </v-formgroup>
-        <v-formgroup v-if="formSettings.sendQueueOpenMsg" class="pt-0">
-          <v-textarea required minlength="3" maxlength="500" v-model="formSettings.queueOpenMsg" />
-        </v-formgroup>
-        <v-formgroup label="Queue close message" class="w-full flex justify-between pr-2 pb-0">
-          <v-switch id="sendQueueCloseMsg" v-model="formSettings.sendQueueCloseMsg" />
-        </v-formgroup>
-        <v-formgroup v-if="formSettings.sendQueueCloseMsg" class="pt-0">
-          <v-textarea required minlength="3" maxlength="500" v-model="formSettings.queueCloseMsg" />
-        </v-formgroup>
-        <v-formgroup label="Current clip message" class="w-full flex justify-between pr-2 pb-0">
-          <v-switch id="sendCurrentClipMsg" v-model="formSettings.sendCurrentClipMsg" />
-        </v-formgroup>
-        <div v-if="formSettings.sendCurrentClipMsg">
-          <div class="text-left pl-1 mb-2 cq-text-subtle">
-            <label>The following will be replaced in the message sent to chat:</label>
-            <ul class="list-disc pl-8">
-              <li><span class="cq-text-subtle-semibold">{url}</span>: url of the clip</li>
-              <li><span class="cq-text-subtle-semibold">{title}</span>: title of the clip</li>
-              <li><span class="cq-text-subtle-semibold">{channel}</span>: channel the clip is of</li>
-              <li><span class="cq-text-subtle-semibold">{game}</span>: game in the clip</li>
-              <li>
-                <span class="cq-text-subtle-semibold">{submitter}</span>: chatter who submitted the clip (or reddit
-                poster)
-              </li>
-            </ul>
-          </div>
-          <v-formgroup class="pt-0">
-            <v-textarea required minlength="3" maxlength="500" v-model="formSettings.currentClipMsg" />
-          </v-formgroup>
-        </div>
-      </div>
-      <v-formgroup>
-        <v-button class="mr-2" type="submit" :disabled="!settings.isModified(formSettings)">Save</v-button>
-        <v-button class="mb-2" type="reset" variant="danger" :disabled="!settings.isModified(formSettings)"
-          >Cancel</v-button
-        >
-      </v-formgroup>
-      <v-alert v-if="showSaveMsg" variant="success">Save successful</v-alert>
     </div>
+    <div class="mt-2">
+      <v-button class="mr-2" type="submit" :disabled="!settings.isModified(formSettings)">Save</v-button>
+      <v-button type="reset" variant="danger" :disabled="!settings.isModified(formSettings)">Cancel</v-button>
+    </div>
+    <v-alert v-if="showSaveMsg" variant="success">Save successful</v-alert>
   </form>
-  <div class="cq-card mx-auto p-2 max-w-lg mt-2">
-    <v-formgroup>
-      <v-button class="w-full" variant="danger" :disabled="clipFinder.cacheEmpty" @click="clipFinder.$reset()">
-        Purge Cache
-      </v-button>
-      <label class="cq-text-subtle"
-        >Twitch clips submitted are cached for future use, this clears all cached clip information.</label
-      >
-    </v-formgroup>
+  <div class="cq-form mt-2">
+    <v-button class="w-full" variant="danger" :disabled="clipFinder.cacheEmpty" @click="clipFinder.$reset()">
+      Purge Cache
+    </v-button>
+    <label class="cq-text-subtle">
+      Twitch clips submitted are cached for future use, this clears all cached clip information.
+    </label>
   </div>
 </template>
 
