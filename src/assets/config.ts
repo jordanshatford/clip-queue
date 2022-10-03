f5286dd0d2a89dd1624b8d5cbff408301c9b6c1d
Jordan Shatford <jordanshatford@live.com>
2022-10-03 14:41:35 -0300

    chore: update list of features for site
    This now includes the ability to block clips from a given
    channel.
    
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -17,6 +17,8 @@ const config = {
       "Popular clips rise up in the queue",
       "Clip removal when user is timed-out, banned, or has message deleted",
       "Allows channel moderators to open/close the queue and move to the next/previous clip",
+      "Add blocked channels to prevent thier clips from being added to the queue.",
+      "Allow channel moderators to modify the blocked channels via chat commands.",
       "Get clips from subreddit posts automatically",
       "Customize settings as you wish",
       "Dark and light UI themes",
