2c5e9dcab2174366d65c7d104fb3a5e7c96dd815
Jordan Shatford <jordanshatford@live.com>
2022-11-14 12:06:39 -0400

    style: unify border styling in application
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-white dark:bg-zinc-800 border-t border-zinc-200 dark:border-zinc-900">
+  <footer class="bg-white dark:bg-zinc-800 border-t border-zinc-300 dark:border-zinc-700">
     <div class="flex flex-col items-center px-8 py-6 mx-auto max-w-6xl sm:flex-row">
       <p class="cq-text">© {{ year }} {{ creator }}</p>
       <span class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
--- src/components/ui/BaseTabs.vue
+++ src/components/ui/BaseTabs.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="mx-auto max-w-xl mb-3 border-b border-zinc-200 dark:border-zinc-700">
+  <div class="mx-auto max-w-xl mb-3 border-b border-zinc-300 dark:border-zinc-700">
     <ul class="cq-text flex flex-wrap -mb-px text-center">
       <li v-for="option in options" :key="option.label" class="mr-2">
         <button
           @click="emit('update:modelValue', option.label)"
           class="inline-flex p-4 border-b-2 group"
-          :class="[
-            option.label === modelValue
-              ? 'text-violet-600 border-violet-600 active dark:text-violet-500 dark:border-violet-500'
-              : 'border-transparent hover:text-zinc-600 hover:border-zinc-300 dark:hover:text-zinc-300 dark:hover:border-zinc-600',
-          ]"
+          :class="[option.label === modelValue ? 'cq-tab-active' : 'cq-tab']"
         >
           <component v-if="option.icon" :is="option.icon" class="w-5 mr-1" />{{ option.label }}
         </button>
