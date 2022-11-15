124190e8bea4d5c10944bf492733603594f0e242
Jordan Shatford <jordanshatford@live.com>
2022-11-15 08:29:48 -0400

    style: simplify button styling
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- src/components/ui/BaseButton.vue
+++ src/components/ui/BaseButton.vue
@@ -1,10 +1,10 @@
 <template>
   <button
-    class="cq-btn"
+    class="disabled:opacity-50 disabled:cursor-not-allowed text-white py-2 px-4 rounded-lg"
     :class="{
-      'cq-btn-danger': variant === 'danger',
-      'cq-btn-primary': variant === 'primary',
-      'cq-btn-brand': variant === 'brand',
+      'bg-red-500 hover:bg-red-400': variant === 'danger',
+      'bg-blue-500 hover:bg-blue-400': variant === 'primary',
+      'bg-violet-500 hover:bg-violet-400': variant === 'brand',
     }"
   >
     <slot></slot>
--- src/components/ui/__tests__/BaseButton.spec.ts
+++ src/components/ui/__tests__/BaseButton.spec.ts
@@ -11,32 +11,28 @@ describe("BaseButton.vue", () => {
 
   it("has a default variant of primary with the correct colors", () => {
     expect(wrapper.vm.variant).toEqual("primary")
-    expect(wrapper.classes()).toContain("cq-btn")
-    expect(wrapper.classes()).toContain("cq-btn-primary")
+    expect(wrapper.classes()).toContain("bg-blue-500")
   })
 
   it("has the correct colors when primary variant is specified", async () => {
     wrapper.setProps({ variant: "primary" })
     await wrapper.vm.$nextTick()
     expect(wrapper.vm.variant).toEqual("primary")
-    expect(wrapper.classes()).toContain("cq-btn")
-    expect(wrapper.classes()).toContain("cq-btn-primary")
+    expect(wrapper.classes()).toContain("bg-blue-500")
   })
 
   it("has the correct colors when brand variant is specified", async () => {
     wrapper.setProps({ variant: "brand" })
     await wrapper.vm.$nextTick()
     expect(wrapper.vm.variant).toEqual("brand")
-    expect(wrapper.classes()).toContain("cq-btn")
-    expect(wrapper.classes()).toContain("cq-btn-brand")
+    expect(wrapper.classes()).toContain("bg-violet-500")
   })
 
   it("has the correct colors when danger variant is specified", async () => {
     wrapper.setProps({ variant: "danger" })
     await wrapper.vm.$nextTick()
     expect(wrapper.vm.variant).toEqual("danger")
-    expect(wrapper.classes()).toContain("cq-btn")
-    expect(wrapper.classes()).toContain("cq-btn-danger")
+    expect(wrapper.classes()).toContain("bg-red-500")
   })
 
   it("emits a click event when clicked", async () => {
