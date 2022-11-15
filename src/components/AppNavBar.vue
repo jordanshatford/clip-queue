bb63c71aaeeefe991d61a21a3958bcca3076be78
Jordan Shatford <jordanshatford@live.com>
2022-11-15 13:33:36 -0400

    fix: show hamburger icon even when user is not logged in
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- src/components/AppNavBar.vue
+++ src/components/AppNavBar.vue
@@ -3,7 +3,7 @@
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <AppHamburger v-if="user.isLoggedIn" v-model="showMobileMenu" />
+          <AppHamburger v-model="showMobileMenu" />
           <AppThemeToggle />
         </div>
         <div class="flex-1 flex items-center justify-center sm:justify-start">
