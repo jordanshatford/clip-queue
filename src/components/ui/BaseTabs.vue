6b2d5efe3d25a532abbd933bbc641ef8d1ec237c
Jordan Shatford <jordanshatford@live.com>
2022-11-17 15:43:31 -0400

    fix: tabs on mobile devices
    closes: #125
    
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- src/components/ui/BaseTabs.vue
+++ src/components/ui/BaseTabs.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="mx-auto max-w-xl mb-3 border-b border-zinc-300 dark:border-zinc-700">
-    <ul class="cq-text flex flex-wrap -mb-px text-center">
+    <ul class="cq-text text-sm md:text-base flex flex-wrap -mb-px text-center">
       <li v-for="option in options" :key="option.label" class="mr-2">
         <button
           @click="emit('update:modelValue', option.label)"
-          class="inline-flex p-4 border-b-2 group"
+          class="flex space-x-1 align-middle p-4 border-b-2 group"
           :class="[option.label === modelValue ? 'cq-tab-active' : 'cq-tab']"
         >
-          <component v-if="option.icon" :is="option.icon" class="w-5 mr-1" />{{ option.label }}
+          <component v-if="option.icon" :is="option.icon" class="w-5" /><span>{{ option.label }}</span>
         </button>
       </li>
     </ul>
