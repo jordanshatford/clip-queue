c3b08c0061cd6c91c27a0ffb852ba88b0ec96f7c
Jordan Shatford <jordanshatford@live.com>
2022-11-17 15:50:30 -0400

    fix: use active-class prop on AppNavBarItem
    closes: #123
    
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- src/components/AppNavBarItem.vue
+++ src/components/AppNavBarItem.vue
@@ -4,6 +4,7 @@
     :to="route.path"
     class="px-3 py-2 my-auto rounded-lg text-zinc-400 dark:text-zinc-300 hover:bg-zinc-100 dark:hover:bg-zinc-700"
     :class="{ block: isMobileMenu }"
+    active-class="text-zinc-700 dark:text-white bg-zinc-100 dark:bg-zinc-900"
   >
     {{ route?.meta?.title }}
   </RouterLink>
