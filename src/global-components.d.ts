d5bc47eb3628e23c5c88c56f604c316316c6cbd4
Jordan Shatford <jordanshatford@live.com>
2023-03-13 20:28:56 -0300

    fix: add typescript definition for global components
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

---
+++ src/global-components.d.ts
@@ -0,0 +1,23 @@
+import BaseAlert from '@/components/ui/BaseAlert.vue'
+import BaseInput from '@/components/ui/BaseInput.vue'
+import BaseSwitch from '@/components/ui/BaseSwitch.vue'
+import BaseTabs from '@/components/ui/BaseTabs.vue'
+import BaseTag from '@/components/ui/BaseTag.vue'
+import BaseTextArea from '@/components/ui/BaseTextArea.vue'
+import BaseButton from '@/components/ui/BaseButton.vue'
+import BaseMultiTagSelect from '@/components/ui/BaseMultiTagSelect.vue'
+import BasePagination from '@/components/ui/BasePagination.vue'
+
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    BaseAlert: typeof BaseAlert
+    BaseInput: typeof BaseInput
+    BaseSwitch: typeof BaseSwitch
+    BaseTabs: typeof BaseTabs
+    BaseTag: typeof BaseTag
+    BaseTextArea: typeof BaseTextArea
+    BaseButton: typeof BaseButton
+    BaseMultiTagSelect: typeof BaseMultiTagSelect
+    BasePagination: typeof BasePagination
+  }
+}
