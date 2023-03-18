403ed30997c025a0093d8eab568acff202d68074
Jordan Shatford <jordanshatford@live.com>
2023-03-18 14:58:09 -0300

    fix: force pagination to bottom of history page
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- src/views/HistoryPage.vue
+++ src/views/HistoryPage.vue
@@ -1,59 +1,61 @@
 <template>
   <p class="cq-title">History</p>
-  <BaseTable :columns="columns" :rows="currentPageClips">
-    <template #head="{ column }">
-      <BaseButton
-        v-if="column.key === 'actions'"
-        :disabled="queue.history.empty()"
-        @click="purgeHistory()"
-        variant="danger"
-        class="flex text-sm float-right"
-        ><TrashIcon class="w-5 mr-1" />Delete All</BaseButton
-      >
-    </template>
-    <template #cell="{ column, row }">
-      <div v-if="column.key === 'info'" class="flex items-center">
-        <img
-          class="hidden sm:block w-24 rounded-xl aspect-video"
-          :src="row.thumbnailUrl"
-          :alt="row.title"
-        />
-        <div class="text-left ml-3 text-sm">
-          <p class="cq-text">
-            {{ row.title }}
-            <span v-if="row.url">
-              <a
-                :href="row.url"
-                target="_blank"
-                rel="noreferrer"
-                className="cq-text-subtle text-lg no-underline hover:text-zinc-600 dark:hover:text-zinc-200"
-              >
-                <ArrowTopRightOnSquareIcon class="w-5 inline-block mb-1" />
-              </a>
-            </span>
-          </p>
-          <p class="cq-text-subtle-semibold">
-            {{ row.channel }}<span class="cq-text-subtle"> playing </span>{{ row.game }}
-          </p>
-        </div>
-      </div>
-      <p v-else-if="column.key === 'submitter'" class="cq-text text-left text-sm">
-        {{ row.submitter }} <span class="cq-text-subtle">({{ row.source }})</span>
-      </p>
-      <div v-else-if="column.key === 'actions'" class="inline-flex mt-2 space-x-2">
+  <div class="min-h-[80vh] flex flex-col justify-between">
+    <BaseTable :columns="columns" :rows="currentPageClips">
+      <template #head="{ column }">
         <BaseButton
-          :disabled="queue.upcoming.includes(row)"
-          @click="queue.add({ ...row, source: ClipSource.HistoryPage }, true)"
+          v-if="column.key === 'actions'"
+          :disabled="queue.history.empty()"
+          @click="purgeHistory()"
+          variant="danger"
+          class="flex text-sm float-right"
+          ><TrashIcon class="w-5 mr-1" />Delete All</BaseButton
         >
-          <PlusIcon class="w-5 h-5" />
-        </BaseButton>
-        <BaseButton variant="danger" @click="queue.removeFromHistory(row)">
-          <TrashIcon class="w-5 h-5" />
-        </BaseButton>
-      </div>
-    </template>
-  </BaseTable>
-  <BasePagination v-model="page" :totalPages="totalPages" class="mt-2" />
+      </template>
+      <template #cell="{ column, row }">
+        <div v-if="column.key === 'info'" class="flex items-center">
+          <img
+            class="hidden sm:block w-24 rounded-xl aspect-video"
+            :src="row.thumbnailUrl"
+            :alt="row.title"
+          />
+          <div class="text-left ml-3 text-sm">
+            <p class="cq-text">
+              {{ row.title }}
+              <span v-if="row.url">
+                <a
+                  :href="row.url"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="cq-text-subtle text-lg no-underline hover:text-zinc-600 dark:hover:text-zinc-200"
+                >
+                  <ArrowTopRightOnSquareIcon class="w-5 inline-block mb-1" />
+                </a>
+              </span>
+            </p>
+            <p class="cq-text-subtle-semibold">
+              {{ row.channel }}<span class="cq-text-subtle"> playing </span>{{ row.game }}
+            </p>
+          </div>
+        </div>
+        <p v-else-if="column.key === 'submitter'" class="cq-text text-left text-sm">
+          {{ row.submitter }} <span class="cq-text-subtle">({{ row.source }})</span>
+        </p>
+        <div v-else-if="column.key === 'actions'" class="inline-flex mt-2 space-x-2">
+          <BaseButton
+            :disabled="queue.upcoming.includes(row)"
+            @click="queue.add({ ...row, source: ClipSource.HistoryPage }, true)"
+          >
+            <PlusIcon class="w-5 h-5" />
+          </BaseButton>
+          <BaseButton variant="danger" @click="queue.removeFromHistory(row)">
+            <TrashIcon class="w-5 h-5" />
+          </BaseButton>
+        </div>
+      </template>
+    </BaseTable>
+    <BasePagination v-model="page" :totalPages="totalPages" class="mt-2" />
+  </div>
 </template>
 
 <script setup lang="ts">
