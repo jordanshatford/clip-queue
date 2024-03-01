d8f665241ba69e5ef2e2b717e4903262620bcf91
Jordan Shatford <jordanshatford@live.com>
2024-03-01 11:44:12 +1100

    chore: update tests to use mock clip data previously added
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- src/components/__tests__/ClipCard.spec.ts
+++ src/components/__tests__/ClipCard.spec.ts
@@ -1,21 +1,12 @@
 import { describe, it, expect } from 'vitest'
 import { shallowMount } from '@vue/test-utils'
+import { clipFromTwitch } from '@/__tests__/mocks'
 import ClipCard from '../ClipCard.vue'
-import { ClipProvider } from '@/interfaces/clips'
 
 describe('ClipCard.vue', () => {
   const wrapper = shallowMount(ClipCard, {
     props: {
-      clip: {
-        id: 'test',
-        title: 'Test title',
-        channel: 'testchannel',
-        game: 'testgame',
-        timestamp: new Date('December 17, 1995 03:24:00').toDateString(),
-        url: 'https://www.twitch.tv/test',
-        thumbnailUrl: 'test',
-        provider: ClipProvider.TWITCH
-      }
+      clip: clipFromTwitch
     }
   })
 
