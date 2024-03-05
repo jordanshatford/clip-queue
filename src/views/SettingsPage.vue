3e6aa5ae883dfc0cd14f455c9dc826f06bb0cfeb
Jordan Shatford <jordanshatford@live.com>
2024-03-05 14:39:22 +1100

    chore: make settings tab not a ref
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -13,16 +13,16 @@ import QueueSettings from '@/components/settings/QueueSettings.vue'
 import ModerationSettings from '@/components/settings/ModerationSettings.vue'
 import OtherSettings from '@/components/settings/OtherSettings.vue'
 
-const tabs = ref([
+const tabs = [
   { label: 'Chat', icon: 'pi pi-comments', view: ChatSettings },
   { label: 'Queue', icon: 'pi pi-list', view: QueueSettings },
   { label: 'Moderation', icon: 'pi pi-flag', view: ModerationSettings },
   { label: 'Other', icon: 'pi pi-cog', view: OtherSettings }
-])
+]
 
 const activeTabIndex = ref<number>(0)
 
 const activeTabView = computed(() => {
-  return tabs.value[activeTabIndex.value].view
+  return tabs[activeTabIndex.value].view
 })
 </script>
