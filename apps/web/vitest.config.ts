f1cd28def8e3dced10e4c5ea2e0bf245d4f4a4be
Jordan Shatford <jordanshatford@live.com>
2024-03-19 14:51:50 +1100

    feat(repo): move vitest testing to repository root using workspace
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,20 +1,15 @@
 import { fileURLToPath } from 'node:url'
-import { mergeConfig, configDefaults, defineConfig } from 'vitest/config'
+import { mergeConfig, configDefaults, defineProject } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
   viteConfig,
-  defineConfig({
+  defineProject({
     test: {
       environment: 'jsdom',
-      watch: false,
       setupFiles: ["src/__tests__/setup.js"],
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
-      coverage: {
-        include: ["src/**"],
-        exclude: ['src/assets/**']
-      }
     }
   })
 )
