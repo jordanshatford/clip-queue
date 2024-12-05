4090a6ebb90348692a6fefa389606a0e687802e0
Jordan Shatford <jordanshatford@live.com>
2024-12-05 09:31:20 -0400

    chore(web/config/player/ui): simplify tailwindcss configs
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,10 +1,6 @@
-import primevue from 'tailwindcss-primeui'
-
 import preset from '@cq/config/tailwind'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  presets: [preset],
-  darkMode: ['selector', '[class*="app-dark"]'],
-  plugins: [primevue]
+  presets: [preset]
 }
