220683ca9f96296b54a5599304a82f80df8c7820
Jordan Shatford <jordanshatford@live.com>
2024-12-09 14:38:26 -0400

    fix(config): issue with import of typescript eslint config
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- packages/config/eslint/vue.js
+++ packages/config/eslint/vue.js
@@ -3,7 +3,7 @@ import prettier from '@vue/eslint-config-prettier'
 import ts from '@vue/eslint-config-typescript'
 import vue from 'eslint-plugin-vue'
 
-import { ignores } from './typescript'
+import { ignores } from './typescript.js'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
