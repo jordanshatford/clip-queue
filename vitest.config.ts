2f3f900b521c3d0cfc8a3660144ecdbf4ae775ac
Jordan Shatford <jordanshatford@live.com>
2025-06-06 10:14:39 -0300

    chore(test): remove deprecated vitest workspace
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    projects: [
+      'apps/*',
+      'packages/*'
+    ],
     coverage: {
       provider: 'v8',
       include: ['apps/**', 'packages/**'],
