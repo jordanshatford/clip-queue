1a567760414ece67a33f6c579f91efe23dc9ab7a
Jordan Shatford <jordanshatford@live.com>
2026-04-30 09:47:39 -0300

    feat: display queue status and number of items in tab
    closes: #496
    
    Display if the queue is open/closed and when open, display
    the number of clips in the queue in the tab title. This allows
    details to be available when the user is not on the tab
    actively.
    
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

---
+++ .changeset/hot-words-bet.md
@@ -0,0 +1,5 @@
+---
+"clip-queue": minor
+---
+
+feat: display queue status and number of items in tab
--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,27 @@
 </template>
 
 <script setup lang="ts">
+import { useTitle } from '@vueuse/core'
 import ConfirmDialog from 'primevue/confirmdialog'
 import Toast from 'primevue/toast'
+import { computed } from 'vue'
 
 import AppFooter from '@/components/AppFooter.vue'
 import AppNavBar from '@/components/AppNavBar.vue'
 import { config } from '@/config'
 
 import { usePreferences } from './stores/preferences'
+import { useQueue } from './stores/queue'
 
 const preferences = usePreferences()
+const queue = useQueue()
+
+const title = computed((): string => {
+  let queueState = queue.isOpen ? 'Open' : 'Closed'
+  if (queue.isOpen) {
+    queueState = `${queueState} (${queue.upcoming.size()})`
+  }
+  return `${queueState} - ${config.title}`
+})
+useTitle(title)
 </script>
