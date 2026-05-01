42fccf70622ccd5dc33e336f00b1537439a9aafd
Jordan Shatford <jordanshatford@live.com>
2026-05-01 14:33:12 -0300

    ci: release
    Fix issue causing vercel ignore build step script to fail.
    
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

--- vercel.sh
+++ vercel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+BRANCH="${VERCEL_GIT_COMMIT_REF:-""}"
 COMMIT_MSG="${VERCEL_GIT_COMMIT_MESSAGE:-""}"
 
 # Only deploy to Vercel if on the main branch with commit message "ci: release".
-if [[ "$BRANCH" == "main" && "$COMMIT_MSG" == "ci: release" ]]; then
+if [[ "$BRANCH" == "main" && "$COMMIT_MSG" == ci:\ release* ]]; then
   exit 1
 fi
 
