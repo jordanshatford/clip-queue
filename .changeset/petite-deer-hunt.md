0c6b4cc86172f653a8bb1dfa9c133d3b476d7b5f
Jordan Shatford <jordanshatford@live.com>
2026-05-11 08:43:54 -0300

    fix: persist queue current and open state using vueuse
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

---
+++ .changeset/petite-deer-hunt.md
@@ -0,0 +1,5 @@
+---
+"clip-queue": patch
+---
+
+fix: persist queue current and open state using vueuse
--- src/stores/queue.ts
+++ src/stores/queue.ts
@@ -1,6 +1,6 @@
 import { useStorage } from '@vueuse/core'
 import { defineStore } from 'pinia'
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 
 import type { Clip } from '@/integrations'
 
@@ -13,207 +13,198 @@ import { useUpcoming } from '@/stores/upcoming'
 
 const DEFAULT_LIMIT: number | null = null
 
-export const useQueue = defineStore(
-  'queue',
-  () => {
-    const logger = useLogger()
+export const useQueue = defineStore('queue', () => {
+  const logger = useLogger()
 
-    const history = useHistory()
-    const upcoming = useUpcoming()
+  const history = useHistory()
+  const upcoming = useUpcoming()
 
-    /**
-     * The limit of clips in the queue.
-     *
-     * @example 10
-     * @note null means no limit.
-     */
-    const limit = useStorage<number | null>('__cq_queue_limit', DEFAULT_LIMIT)
-    const isOpen = ref<boolean>(true)
-    const current = ref<Clip | undefined>(undefined)
+  /**
+   * The limit of clips in the queue.
+   *
+   * @example 10
+   * @note null means no limit.
+   */
+  const limit = useStorage<number | null>('__cq_queue_limit', DEFAULT_LIMIT)
+  const isOpen = useStorage<boolean>('__cq_queue_isopen', true)
+  const current = useStorage<Clip | undefined>('__cq_queue_current', undefined)
 
-    function clear() {
-      logger.info('[Queue]: Clearing upcoming queue.')
-      upcoming.reset()
-    }
+  function clear() {
+    logger.info('[Queue]: Clearing upcoming queue.')
+    upcoming.reset()
+  }
 
-    function purge() {
-      logger.info('[Queue]: Purging queue history.')
-      history.reset()
-    }
+  function purge() {
+    logger.info('[Queue]: Purging queue history.')
+    history.reset()
+  }
 
-    function add(clip: Clip, force = false) {
-      // Force add the clip
-      if (force) {
-        upcoming.add(clip)
-        return
-      }
-      // Ignore clip when queue isnt open
-      if (!isOpen.value) {
-        return
-      }
-      // Ignore when we have previously watched it
-      const hasBeenWatched =
-        (current.value && toClipUUID(current.value) === toClipUUID(clip)) || history.includes(clip)
-      if (hasBeenWatched) {
-        return
-      }
-      // Queue is full based on limit
-      if (limit.value && upcoming.length >= limit.value) {
-        // If the clip is already in the queue add it so submitters is updated
-        if (!upcoming.includes(clip)) {
-          return
-        }
-      }
+  function add(clip: Clip, force = false) {
+    // Force add the clip
+    if (force) {
       upcoming.add(clip)
+      return
     }
-
-    function remove(clip: Clip) {
-      upcoming.remove(clip)
+    // Ignore clip when queue isnt open
+    if (!isOpen.value) {
+      return
     }
-
-    function play(clip: Clip) {
+    // Ignore when we have previously watched it
+    const hasBeenWatched =
+      (current.value && toClipUUID(current.value) === toClipUUID(clip)) || history.includes(clip)
+    if (hasBeenWatched) {
+      return
+    }
+    // Queue is full based on limit
+    if (limit.value && upcoming.length >= limit.value) {
+      // If the clip is already in the queue add it so submitters is updated
       if (!upcoming.includes(clip)) {
         return
       }
-      if (current.value?.id) {
-        history.add(current.value)
-      }
-      upcoming.remove(clip)
-      current.value = clip
     }
+    upcoming.add(clip)
+  }
 
-    function open() {
-      isOpen.value = true
+  function remove(clip: Clip) {
+    upcoming.remove(clip)
+  }
+
+  function play(clip: Clip) {
+    if (!upcoming.includes(clip)) {
+      return
     }
-
-    function close() {
-      isOpen.value = false
+    if (current.value?.id) {
+      history.add(current.value)
     }
+    upcoming.remove(clip)
+    current.value = clip
+  }
 
-    function previous() {
-      if (current.value?.id) {
-        upcoming.unshift(current.value)
-      }
-      current.value = history.pop()
+  function open() {
+    isOpen.value = true
+  }
+
+  function close() {
+    isOpen.value = false
+  }
+
+  function previous() {
+    if (current.value?.id) {
+      upcoming.unshift(current.value)
     }
+    current.value = history.pop()
+  }
 
-    function next() {
-      if (current.value?.id) {
-        history.add(current.value)
-      }
-      current.value = upcoming.shift()
+  function next() {
+    if (current.value?.id) {
+      history.add(current.value)
     }
+    current.value = upcoming.shift()
+  }
 
-    /**
-     * Determine if the settings are modified.
-     */
-    const isSettingsModified = computed(() => {
-      return limit.value !== DEFAULT_LIMIT
-    })
+  /**
+   * Determine if the settings are modified.
+   */
+  const isSettingsModified = computed(() => {
+    return limit.value !== DEFAULT_LIMIT
+  })
 
-    /**
-     * Reset settings related to this store.
-     */
-    function resetSettings(): void {
-      limit.value = DEFAULT_LIMIT
-    }
+  /**
+   * Reset settings related to this store.
+   */
+  function resetSettings(): void {
+    limit.value = DEFAULT_LIMIT
+  }
 
-    /**
-     * Register commands for the queue.
-     */
-    useCommands().register(
-      {
-        id: 'open',
-        aliases: ['o'],
-        help: {
-          description: m.command_open,
-        },
-        execute: () => {
-          open()
-        },
+  /**
+   * Register commands for the queue.
+   */
+  useCommands().register(
+    {
+      id: 'open',
+      aliases: ['o'],
+      help: {
+        description: m.command_open,
       },
-      {
-        id: 'close',
-        aliases: ['c'],
-        help: {
-          description: m.command_close,
-        },
-        execute: () => {
-          close()
-        },
+      execute: () => {
+        open()
       },
-      {
-        id: 'previous',
-        aliases: ['back', 'prev', 'last'],
-        help: {
-          description: m.command_previous,
-        },
-        execute: () => {
-          previous()
-        },
-      },
-      {
-        id: 'next',
-        aliases: ['forward'],
-        help: {
-          description: m.command_next,
-        },
-        execute: () => {
-          next()
-        },
-      },
-      {
-        id: 'setlimit',
-        aliases: ['limit'],
-        help: {
-          args: [m.number],
-          description: m.command_set_limit,
-        },
-        execute: ({ args }) => {
-          if (args[0]) {
-            const l = Number.parseInt(args[0])
-            if (Number.isNaN(l) || l < 1) {
-              return
-            }
-            limit.value = l
-          }
-        },
-      },
-      {
-        id: 'removelimit',
-        aliases: ['rmlimit'],
-        help: {
-          description: m.command_remove_limit,
-        },
-        execute: () => {
-          limit.value = null
-        },
-      },
-    )
-
-    return {
-      limit,
-      isOpen,
-      history,
-      current,
-      upcoming,
-      clear,
-      purge,
-      add,
-      remove,
-      play,
-      open,
-      close,
-      previous,
-      next,
-      isSettingsModified,
-      resetSettings,
-    }
-  },
-  {
-    persist: {
-      key: 'cq-queue',
-      pick: ['current', 'isOpen'],
     },
-  },
-)
+    {
+      id: 'close',
+      aliases: ['c'],
+      help: {
+        description: m.command_close,
+      },
+      execute: () => {
+        close()
+      },
+    },
+    {
+      id: 'previous',
+      aliases: ['back', 'prev', 'last'],
+      help: {
+        description: m.command_previous,
+      },
+      execute: () => {
+        previous()
+      },
+    },
+    {
+      id: 'next',
+      aliases: ['forward'],
+      help: {
+        description: m.command_next,
+      },
+      execute: () => {
+        next()
+      },
+    },
+    {
+      id: 'setlimit',
+      aliases: ['limit'],
+      help: {
+        args: [m.number],
+        description: m.command_set_limit,
+      },
+      execute: ({ args }) => {
+        if (args[0]) {
+          const l = Number.parseInt(args[0])
+          if (Number.isNaN(l) || l < 1) {
+            return
+          }
+          limit.value = l
+        }
+      },
+    },
+    {
+      id: 'removelimit',
+      aliases: ['rmlimit'],
+      help: {
+        description: m.command_remove_limit,
+      },
+      execute: () => {
+        limit.value = null
+      },
+    },
+  )
+
+  return {
+    limit,
+    isOpen,
+    history,
+    current,
+    upcoming,
+    clear,
+    purge,
+    add,
+    remove,
+    play,
+    open,
+    close,
+    previous,
+    next,
+    isSettingsModified,
+    resetSettings,
+  }
+})
