72335d71ae326e3d9ab46d52670d9278dd290d9c
Jordan Shatford <jordanshatford@live.com>
2026-05-30 14:19:04 -0300

    feat: show integration icon when hovering avatar
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

---
+++ .changeset/goofy-goats-clean.md
@@ -0,0 +1,5 @@
+---
+"clip-queue": minor
+---
+
+feat: show integration icon when hovering avatar
--- app/components/integration/Avatars.vue
+++ app/components/integration/Avatars.vue
@@ -1,6 +1,12 @@
 <template>
   <UAvatarGroup size="sm">
     <UTooltip v-for="avatar of avatars" :key="avatar.src" :text="avatar.alt">
+      <template #content>
+        <div class="flex items-center gap-1">
+          <IntegrationIcon :id="avatar.integration" />
+          <span>{{ avatar.alt }}</span>
+        </div>
+      </template>
       <UAvatar v-bind="avatar" />
     </UTooltip>
   </UAvatarGroup>
@@ -9,14 +15,17 @@
 <script setup lang="ts">
 import type { AvatarProps } from '@nuxt/ui'
 
+import type { IntegrationID } from '~/integrations'
+
 import { toColor } from '~/integrations/core'
 
 const integrations = useIntegrations()
 
-const avatars = computed<AvatarProps[]>(() => {
+const avatars = computed<(AvatarProps & { integration: IntegrationID })[]>(() => {
   return integrations.integrations
     .filter((integration) => integration.authentication?.isLoggedIn)
     .map((integration) => ({
+      integration: integration.id,
       src: integration.authentication?.user?.profileImageURL,
       loading: 'lazy',
       alt: integration.authentication?.user?.name,
