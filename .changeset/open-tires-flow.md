ec9a10095a94453983062b8db6e982955b7ea2d8
Jordan Shatford <jordanshatford@live.com>
2026-06-22 16:06:14 -0300

    fix: properly await oauth validation and token refresh
    Signed-off-by: Jordan Shatford <jordanshatford@live.com>

---
+++ .changeset/open-tires-flow.md
@@ -0,0 +1,5 @@
+---
+"clip-queue": patch
+---
+
+fix: properly await oauth validation and token refresh
--- server/api/twitch/oath/validate.post.ts
+++ server/api/twitch/oath/validate.post.ts
@@ -10,7 +10,7 @@ export default defineEventHandler(async (event): Promise<OAuthResponse> => {
   const client = new TwitchOAuthClient(event)
 
   try {
-    client.getValidatedTokenInfo()
+    await client.getValidatedTokenInfo()
   } catch (error) {
     // If the error isn't a 401, it's a network/server issue. Bubble it up.
     if (!(error instanceof FetchError) || error.statusCode !== 401) {
@@ -18,7 +18,7 @@ export default defineEventHandler(async (event): Promise<OAuthResponse> => {
     }
     // If Twitch is telling us the token is not valid, attempt to refresh the token
     // and update the user session based on the new values.
-    client.refreshToken()
+    await client.refreshToken()
   }
 
   const user = await client.getCurrentUser()
